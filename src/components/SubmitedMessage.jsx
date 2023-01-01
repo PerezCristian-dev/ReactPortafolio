913203a78fcc453af8f48540e637ffef02d8ddc3
Cristian Perez <chrisperezps4@hotmail.com>
2023-01-01 14:16:31 -0400

    fixing image size

--- src/components/SubmitedMessage.jsx
+++ src/components/SubmitedMessage.jsx
@@ -8,12 +8,12 @@ export const SubmitedMessage = () => {
   return (
 
       <div className="flex flex-col justify-center items-center text-center">
-        <img className="w-40 animate__bounceIn" src="../checkmark.png" alt="success" />
+        <img className="w-36 animate__bounceIn" src="../checkmark.png" alt="success" />
         <h2 className="text-3xl my-4 font-bold animate__animated animate__bounceIn ">
-          Your email send was sent successfuly
+          Your email was sent successfuly
         </h2>
         <p className="animate__animated animate__bounceIn">
-          Thank you for your interest in getting touch with me, I will review
+          Thank you for your interest in working with me, I will review
           your email and get back to you as soon as possible.
         </p>
       </div>
